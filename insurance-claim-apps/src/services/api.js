0ab0eff72a5ee08820837305f5e88fffe0445f6a
Nihala-ms <nihalabintsalih@gmail.com>
2026-09-24 23:39:25 +0530

    frontend for deployment

--- insurance-claim-apps/src/services/api.js
+++ insurance-claim-apps/src/services/api.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:3000/api";
-
+const API_URL =
+  "https://insurance-management-systems-pht3uta6d-nihala-ms-projects.vercel.app/api";
 // =====================================================
 // HELPER
 // =====================================================
